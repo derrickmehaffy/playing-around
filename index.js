8332d0775ba38d47999a321e057fd7f6707d7915
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-08-21 10:14:48 -0700

    Debugging write data to a file

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const axios = require('axios');
+const fs = require('fs');
 
 let sites = {
-  apsites: [],
-  twsites: []
+	apsites: [],
+	bmsites: [],
+	btsites: [],
+	cssites: [],
+	fgsites: [],
+	fmsites: [],
+	gvsites: [],
+	gysites: [],
+	lssites: [],
+	tbsites: [],
+	twsites: [],
 };
 
 const API_ENDPOINT = `https://api.canonn.tech:2053`;
@@ -16,42 +26,42 @@ const capi = axios.create({
 	},
 });
 
-function prettyJSON(obj) {
-	console.log(JSON.stringify(obj, null, 2));
-}
+const go = async types => {
+	let typeKeys = Object.keys(types);
+	// loop through types to get all the data
+	for (i = 0; i < typeKeys.length; i++) {
+		console.log('Fetching ' + typeKeys[i]);
+		sites[typeKeys[i]] = await getSites(typeKeys[i]);
+	}
+	fs.writeFile('data/data.json', JSON.stringify(sites, null, 2), err => {
+		if (err) throw err;
+		console.log('Saved to data/data.json');
+	});
+};
 
-const go = async (types) => {
-
-  let typeKeys = Object.keys(types)
-  // loop through types to get all the data
-  for (i=0; i < typeKeys.length; i++) {
-    sites[typeKeys[i]] = await getSites(typeKeys[i]);
-    };
-  //sites = await getSites(types[0]);
-  prettyJSON(sites);
-}
-
-const getSites = async (type) => {
+const getSites = async type => {
 	let records = [];
 	let keepGoing = true;
 	let API_START = 0;
 	while (keepGoing) {
-    let response = await reqSites(API_START, type);
+		let response = await reqSites(API_START, type);
 
-    //trying to return `apsites`
-    let responseKeys = Object.keys(response.data.data);
+		//trying to return `apsites`
+		let responseKeys = Object.keys(response.data.data);
 
-    // push data into array under `apsites`
-    await records.push.apply(records, response.data.data[responseKeys[0]]);
-    
-    API_START += API_LIMIT;
-    
-    // if more data under apsites then keep asking for it
+		// push data into array under `apsites`
+		await records.push.apply(records, response.data.data[responseKeys[0]]);
+
+		let page = 1;
+		API_START += API_LIMIT;
+		page += 1;
+
+		// if more data under apsites then keep asking for it
 		if (response.data.data[responseKeys[0]].length < API_LIMIT) {
 			keepGoing = false;
 			return records;
 		}
-  }
+	}
 };
 
 const reqSites = async (API_START, type) => {
