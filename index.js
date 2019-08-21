5ff1d688823d7c9ea10a25376c5c9eafd2d1459e
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-08-21 07:51:56 -0700

    refactoring

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const axios = require('axios');
 
+let types = [
+  "ap",
+  "tw"
+];
+let sites = [];
+
+const API_ENDPOINT = `https://api.canonn.tech:2053`;
+const API_LIMIT = 10;
+
 const capi = axios.create({
-	baseURL: 'https://api.canonn.tech:2053',
+	baseURL: API_ENDPOINT,
 	headers: {
 		'Content-Type': 'application/json',
 		Accept: 'application/json',
@@ -9,23 +18,47 @@ const capi = axios.create({
 });
 
 function prettyJSON(obj) {
-  console.log(JSON.stringify(obj, null, 2));
+	console.log(JSON.stringify(obj, null, 2));
 }
 
-function extend(obj, src) {
-  Object.keys(src).forEach(function(key) { obj[key] = src[key]; });
-  return obj;
-}
-
-async function capiGet(start, limit, where, type) {
+const go = async (types) => {
+  // loop through types to get all the data
   /*
-  let start = 0;
-  let limit = 1000;
-  let where = {};
-  let type = 'ap'
+  for (i=0; i < types.length; i++) {
+    sites = await getSites(types[i]);
+  }
   */
-  let typeQuery = type + 'sites';
-  let query = `query ($limit:Int, $start:Int, $where:JSON){ 
+  sites = await getSites(types[0]);
+  prettyJSON(sites);
+}
+
+const getSites = async (type) => {
+	let records = [];
+	let keepGoing = true;
+	let API_START = 0;
+	while (keepGoing) {
+    let response = await reqSites(API_START, type);
+
+    //trying to return `apsites`
+    let responseKeys = Object.keys(response.data.data);
+
+    // push data into array under `apsites`
+    await records.push.apply(records.responseKeys[0], response.data.data.responseKeys[0]);
+    
+    API_START += API_LIMIT;
+    
+    // if more data under apsites then keep asking for it
+		if (response.data.data.responseKeys[0].length < API_LIMIT) {
+			keepGoing = false;
+			return records;
+		}
+  }
+};
+
+const reqSites = async (API_START, type) => {
+	let typeQuery = type + 'sites';
+	let where = {};
+	let query = `query ($limit:Int, $start:Int, $where:JSON){ 
     ${typeQuery} (limit: $limit, start: $start, where: $where){ 
       system{ 
         systemName
@@ -40,31 +73,20 @@ async function capiGet(start, limit, where, type) {
     }
   }`;
 
-  let response = await capi({
-    url: '/graphql',
-    method: 'post',
-    data: {
-      query,
-      variables: {
-        start,
-        limit,
-        where,
-      },
-    },
-  });
+	let payload = await capi({
+		url: '/graphql',
+		method: 'post',
+		data: {
+			query,
+			variables: {
+				start: API_START,
+				limit: API_LIMIT,
+				where,
+			},
+		},
+	});
 
-  capiResponse = extend(await response.data.data, capiResponse);
-}
-
-let capiResponse = {};
-
-async function capiData() {
-
-  await capiGet(0, 5, {}, 'ap');
-  await capiGet(0, 5, {}, 'bm');
-  await capiGet(5, 5, {}, 'bm');
-  prettyJSON(capiResponse);
+	return payload;
 };
 
-capiData();
-
+go(types);
